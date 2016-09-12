cee695cb7899b6f5b8cbc9e7bed7e7e1a2386e9f
Toshiki Takeuchi <take@xcoo.jp>
2016-09-12 14:40:55 +0900

    Modify height of bottom tool bars

--- Upupu/Views/BlackToolBar.swift
+++ Upupu/Views/BlackToolBar.swift
@@ -11,17 +11,36 @@ import UIKit
 
 class BlackToolBar: UIToolbar {
 
+    private var topBorderEnabled = false
+    private var bottomBorderEnabled = false
+
+    init(topBorderEnabled: Bool = false, bottomBorderEnabled: Bool = false) {
+        super.init(frame: CGRect.zero)
+        self.topBorderEnabled = topBorderEnabled
+        self.bottomBorderEnabled = bottomBorderEnabled
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func drawRect(rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
 
         CGContextSetFillColorWithColor(context, UIColor.blackColor().CGColor)
         CGContextFillRect(context, bounds)
 
-        CGContextSetFillColorWithColor(context, UIColor(white: 0.1, alpha: 1.0).CGColor)
-        CGContextFillRect(context, CGRect.init(x: 0.0, y: 0.0,
-            width: bounds.size.width, height: 2.0))
-        CGContextFillRect(context, CGRect.init(x: 0.0, y: bounds.size.height,
-            width: bounds.size.width, height: bounds.size.height))
+        CGContextSetFillColorWithColor(context, UIColor(white: 0.2, alpha: 1.0).CGColor)
+
+        if topBorderEnabled {
+            CGContextFillRect(context, CGRect.init(x: 0.0, y: 0.0,
+                width: bounds.size.width, height: 1.0))
+        }
+
+        if bottomBorderEnabled {
+            CGContextFillRect(context, CGRect.init(x: 0.0, y: bounds.size.height - 2.0,
+                width: bounds.size.width, height: 1.0))
+        }
     }
 
 }
