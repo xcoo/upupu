fd9b31edab0e8f199c608c9a0b7e543ab98eba02
Toshiki Takeuchi <take@xcoo.jp>
2016-11-07 16:02:11 +0900

    Remove an extra empty line

--- Upupu/AppDelegate.swift
+++ Upupu/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         setupDefaults()
