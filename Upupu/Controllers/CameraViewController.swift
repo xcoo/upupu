e6b2829fc03179542334e686614e04b98fbb941c
Toshiki Takeuchi <take@xcoo.jp>
2017-04-04 18:21:34 +0900

    Fix warning of notification removal

--- Upupu/Controllers/CameraViewController.swift
+++ Upupu/Controllers/CameraViewController.swift
@@ -79,7 +79,7 @@ UIImagePickerControllerDelegate, UIAccelerometerDelegate {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         CameraHelper.shared.stopRunning()
-        NotificationCenter.default.removeObserver(self)
+        NotificationCenter.default.removeObserver(self, name: .UIDeviceOrientationDidChange, object: nil)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -113,7 +113,7 @@ UIImagePickerControllerDelegate, UIAccelerometerDelegate {
         UIDevice.current.beginGeneratingDeviceOrientationNotifications()
         NotificationCenter.default
             .addObserver(self, selector: #selector(deviceOrientationDidChange),
-                         name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+                         name: .UIDeviceOrientationDidChange, object: nil)
     }
 
     private func resetButtons() {
