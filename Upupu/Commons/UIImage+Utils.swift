67e9d3eb24575485cbe3e951d5fba13038c702bb
Toshiki Takeuchi <take@xcoo.jp>
2017-06-12 19:42:46 +0900

    Fix image scaling issue

--- Upupu/Commons/UIImage+Utils.swift
+++ Upupu/Commons/UIImage+Utils.swift
@@ -12,26 +12,26 @@ import UIKit
 extension UIImage {
 
     private func calcTransform(_ size: CGSize, orientation: UIImageOrientation) -> CGAffineTransform {
-            switch orientation {
-            case .up:
-                return CGAffineTransform.identity
-            case .upMirrored:
-                return CGAffineTransform(translationX: size.width, y: 0).scaledBy(x: -1.0, y: 1.0)
-            case .down:
-                return CGAffineTransform(translationX: size.width, y: size.height).rotated(by: .pi)
-            case .downMirrored:
-                return CGAffineTransform(translationX: 0, y: size.height).scaledBy(x: 1.0, y: -1.0)
-            case .leftMirrored:
-                return CGAffineTransform(translationX: size.height, y: size.width)
-                    .scaledBy(x: -1.0, y: 1.0)
-                    .rotated(by: 3.0 * .pi / 2.0)
-            case .left:
-                return CGAffineTransform(translationX: 0, y: size.width).rotated(by: 3.0 * .pi / 2.0)
-            case .rightMirrored:
-                return CGAffineTransform(scaleX: -1.0, y: 1.0).rotated(by: .pi / 2.0)
-            case .right:
-                return CGAffineTransform(translationX: size.height, y: 0).rotated(by: .pi / 2.0)
-            }
+        switch orientation {
+        case .up:
+            return CGAffineTransform.identity
+        case .upMirrored:
+            return CGAffineTransform(translationX: size.width, y: 0).scaledBy(x: -1.0, y: 1.0)
+        case .down:
+            return CGAffineTransform(translationX: size.width, y: size.height).rotated(by: .pi)
+        case .downMirrored:
+            return CGAffineTransform(translationX: 0, y: size.height).scaledBy(x: 1.0, y: -1.0)
+        case .leftMirrored:
+            return CGAffineTransform(translationX: size.height, y: size.width)
+                .scaledBy(x: -1.0, y: 1.0)
+                .rotated(by: 3.0 * .pi / 2.0)
+        case .left:
+            return CGAffineTransform(translationX: 0, y: size.width).rotated(by: 3.0 * .pi / 2.0)
+        case .rightMirrored:
+            return CGAffineTransform(scaleX: -1.0, y: 1.0).rotated(by: .pi / 2.0)
+        case .right:
+            return CGAffineTransform(translationX: size.height, y: 0).rotated(by: .pi / 2.0)
+        }
     }
 
     func scaledImage(_ size: CGSize) -> UIImage? {
@@ -40,22 +40,20 @@ extension UIImage {
         }
 
         let currentSize = CGSize.init(width: imageRef.width, height: imageRef.height)
+        let currentRatio = CGFloat(currentSize.width / currentSize.height)
+
+        let sizeRatio = CGFloat(size.width / size.height)
 
         var scalingSize = size
-        if currentSize.width > currentSize.height {
-            if size.width < size.height {
-                scalingSize.width = size.height
-                scalingSize.height = size.width
-            }
+
+        if currentRatio > sizeRatio {
+            scalingSize.height = size.width / currentRatio
         } else {
-            if size.width > size.height {
-                scalingSize.width = size.height
-                scalingSize.height = size.width
-            }
+            scalingSize.width = size.height * currentRatio
         }
 
-        let scalingRatioW = Float(scalingSize.width) / Float(currentSize.width)
-        let scalingRatioH = Float(scalingSize.height) / Float(currentSize.height)
+        let scalingRatioW = scalingSize.width / currentSize.width
+        let scalingRatioH = scalingSize.height / currentSize.height
         let transform = calcTransform(currentSize, orientation: imageOrientation)
 
         var newSize = CGSize.init(width: scalingSize.width, height: scalingSize.height)
@@ -72,11 +70,11 @@ extension UIImage {
         let context = UIGraphicsGetCurrentContext()
 
         if imageOrientation == .right || imageOrientation == .left {
-            context?.scaleBy(x: -CGFloat(scalingRatioW), y: CGFloat(scalingRatioH))
-            context?.translateBy(x: -CGFloat(currentSize.height), y: 0)
+            context?.scaleBy(x: -scalingRatioW, y: scalingRatioH)
+            context?.translateBy(x: -currentSize.height, y: 0)
         } else {
-            context?.scaleBy(x: CGFloat(scalingRatioW), y: -CGFloat(scalingRatioH))
-            context?.translateBy(x: 0, y: -CGFloat(currentSize.height))
+            context?.scaleBy(x: scalingRatioW, y: -scalingRatioH)
+            context?.translateBy(x: 0, y: -currentSize.height)
         }
 
         context?.concatenate(transform)
